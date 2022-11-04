e2add4c7763f2f0fbd62d28f4d01370107cbe0fc
Park Sang-Yeop <112646715+2201204@users.noreply.github.com>
2022-11-05 01:00:40 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,27 @@
+삼각형
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.left(45)
+t.forward(141)
+t.up()
+t.goto(0,0)
+t.down()
+t.right(45)
+t.forward(100)
+t.left(90)
+t.forward(100)
+점의 길이
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+x1=int(input("x1:"))
+y1=int(input("y1:"))
+x2=int(input("x2:"))
+y2=int(input("y2:"))
+z=(((x1-x2)**2)+((y1-y2)**2))**0.5)
+t.up()
+t.goto(x1,y1)
+t.down()
+t.goto(x2,y2)
+t.write("점의 길이="+str(z))
